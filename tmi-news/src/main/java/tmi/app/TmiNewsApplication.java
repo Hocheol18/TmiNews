4731932638e5a1de26043a7d8579d058f5faa4c6
chico <silverstuff@naver.com>
2025-03-13 19:05:14 +0900

    first commit

---
+++ tmi-news/src/main/java/tmi/app/TmiNewsApplication.java
@@ -0,0 +1,13 @@
+package tmi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmiNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmiNewsApplication.class, args);
+	}
+
+}
