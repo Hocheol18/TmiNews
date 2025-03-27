042d9ca1b5b11d9cfa9e6f99a8fdba591574cb2d
백지윤 <wendy6992@naver.com>
2025-03-27 19:14:50 +0900

    Fix: 기존 BE 폴더 삭제

--- tmi-news/src/main/java/tmi/app/exception/InvalidKakaoAuthCodeException.java
+++
@@ -1,7 +0,0 @@
-package tmi.app.exception;
-
-public class InvalidKakaoAuthCodeException extends RuntimeException {
-    public InvalidKakaoAuthCodeException(String message) {
-        super(message);
-    }
-}
